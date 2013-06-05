149b8d843c7b8904ea59d7914827caa9885e0a3f
加藤 史也 <FumKATO@katou-no-MacBook-Air.local>
2013-06-05 18:20:02 +0900

    Add build command.

--- src/main/scala/net/aicomp/entity/Game.scala
+++ src/main/scala/net/aicomp/entity/Game.scala
@@ -25,6 +25,14 @@ class Game extends CommandValidation {
     val t = args(2)
     map.build(new Point(x, y), t)
   }
+  
+  //TODO
+  def buildCommand(args: List[String]) {
+    val x = args(0).toInt
+    val y = args(1).toInt
+    val t = args(2)
+    map.build(new Point(x, y), t)
+  }
 
   //TODO
   def buildCommand(args: List[String]) {
