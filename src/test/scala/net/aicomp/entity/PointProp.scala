cd2bf3892df908c039f025522263c85c542be802
Kazunori Sakamoto <exkazuu@gmail.com>
2013-06-08 19:13:07 +0900

    Add more property tests.

--- src/test/scala/net/aicomp/entity/PointProp.scala
+++ src/test/scala/net/aicomp/entity/PointProp.scala
@@ -8,12 +8,31 @@ import org.scalacheck.Gen
 object PointProp extends Properties("Point") {
   val field = new Field(7)
 
-  property("shortestPathTo") = forAll { (p1: Point, p2: Point) =>
-    if (p1.within(field.radius) && p2.within(field.radius))
-      p1.shortestPathTo(p2, field).get.size == p1.distance(p2)
-    else
-      p1.shortestPathTo(p2, field) == None
+  property("shortestPathTo") = forAll(pointWithin, pointWithin) { (p1: Point, p2: Point) =>
+    p1.shortestPathTo(p2, field).get.size == p1.distance(p2)
   }
+
+  property("shortestPathTo") = forAll(pointWithin | pointWithout, pointWithout) { (p1: Point, p2: Point) =>
+    p1.shortestPathTo(p2, field) == None
+  }
+
+  property("shortestPathTo") = forAll(pointWithout, pointWithin | pointWithout) { (p1: Point, p2: Point) =>
+    p1.shortestPathTo(p2, field) == None
+  }
+
+  property("shortestPathTo") = forAll(pointWithout, pointWithout) { (p1: Point, p2: Point) =>
+    p1.shortestPathTo(p2, field) == None
+  }
+
+  def pointWithin = (for {
+    x <- Gen.choose(-field.radius, field.radius)
+    y <- Gen.choose(-field.radius, field.radius)
+  } yield Point(x, y)).filter(p => p.within(field.radius))
+
+  def pointWithout = (for {
+    x <- Gen.choose(-field.radius * 2, field.radius * 2)
+    y <- Gen.choose(-field.radius * 2, field.radius * 2)
+  } yield Point(x, y)).filter(p => !p.within(field.radius))
 
   implicit def arbPoint: Arbitrary[Point] =
     Arbitrary {
