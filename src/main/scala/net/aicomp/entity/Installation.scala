6f2feb8ebb12468c89534e9b52d3404c1a04ebe1
irxground <irxnjhtchlnrw@gmail.com>
2013-06-09 16:23:18 +0900

    Require robot to build inst. close #56

--- src/main/scala/net/aicomp/entity/Installation.scala
+++ src/main/scala/net/aicomp/entity/Installation.scala
@@ -1,19 +1,19 @@
 package net.aicomp.entity
 
 object Installation {
-  val initial = new Installation("initial")
-  val br = new Installation("br")
-  val sh = new Installation("sh")
-  val at = new Installation("at")
-  val mt = new Installation("mt")
-  val pk = new Installation("pk")
-  val sq = new Installation("sq")
-  val pl = new Installation("pl")
+  val initial = new Installation("initial", 0)
+  val br = new Installation("br", 1)
+  val sh = new Installation("sh", 2)
+  val at = new Installation("at", 3)
+  val mt = new Installation("mt", 4)
+  val pk = new Installation("pk", 5)
+  val sq = new Installation("sq", 6)
+  val pl = new Installation("pl", 7)
 
   val buildables = List(br, sh, at, mt, pk, sq, pl)
 }
 
-@cloneable class Installation(val name: String) {
+@cloneable class Installation(val name: String, val cost: Int) {
   def head = name.head
 
   override def toString = name
