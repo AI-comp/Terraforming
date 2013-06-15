043d003d0c93bd1956bdd4118c328249d6d2e902
Kazunori SAKAMOTO <exkazuu@gmail.com>
2013-06-16 00:25:47 +0900

    Remove .classpath. Divide Input.scala to two files.

--- src/main/scala/net/aicomp/entity/input/Input.scala
+++ src/main/scala/net/aicomp/entity/input/Input.scala
@@ -1,11 +1,12 @@
 package net.aicomp.entity.input
 
 import java.util.Scanner
+
+import scala.collection.mutable.ListBuffer
+
 import net.aicomp.entity.Game
 import net.aicomp.scene.graphic.TextBoxScene
 import net.exkazuu.gameaiarena.player.ExternalComputerPlayer
-import net.exkazuu.gameaiarena.runner.AbstractRunner
-import scala.collection.mutable.ListBuffer
 
 trait Input {
   def inputCommandLists(game: Game): Array[Array[String]]
@@ -37,22 +38,4 @@ class ExternalProgramInput(commands: Array[String]) extends ExternalComputerPlay
     }
     buf.map(_.split(" ").toArray).toArray
   }
-}
-
-class Manipulator(input: Input) extends AbstractRunner[Game, Array[Array[String]], Input] {
-  private var _game: Game = null
-  private var _commands: Array[Array[String]] = Array()
-
-  override def getComputerPlayer = input
-
-  override def runPreProcessing(game: Game) {
-    _game = game
-    _commands = Array()
-  }
-
-  override def runProcessing() {
-    _commands = input.inputCommandLists(_game)
-  }
-
-  override def runPostProcessing() = _commands
 }
