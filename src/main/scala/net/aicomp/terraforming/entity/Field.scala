4a0ca778a1201c6f29d48aed01bb0e17f3d597fc
Kazunori SAKAMOTO <exkazuu@gmail.com>
2013-07-08 00:57:21 +0900

    Fix attack power.

--- src/main/scala/net/aicomp/terraforming/entity/Field.scala
+++ src/main/scala/net/aicomp/terraforming/entity/Field.scala
@@ -124,7 +124,7 @@ class Field(val radius: Int, val tiles: Map[Point, Tile]) {
         tile.installation match {
           case Some(Installation.attack) =>
             availableLineTiles(p).foreach { t =>
-              t.robots = math.max(t.robots - 2, 0)
+              t.robots = math.max(t.robots - 3, 0)
             }
           case _ =>
         }
