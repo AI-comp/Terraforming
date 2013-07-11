c603982f19637718ec3bc7cee18024ae4a09bef6
Kazunori SAKAMOTO <exkazuu@gmail.com>
2013-07-11 22:05:13 +0900

    Add an applet class in Scala.

---
+++ src/main/scala/net/aicomp/terraforming/AppletMain2.scala
@@ -0,0 +1,12 @@
+package net.aicomp.terraforming
+
+import java.applet.Applet
+
+import scala.util.control.Exception.allCatch
+
+class AppletMain2 extends Applet {
+  override def start() {
+    val fps = allCatch opt getParameter("fps").toDouble getOrElse (15.0)
+    Main.main(Array("-" + Main.FPS, fps.toString));
+  }
+}
