0b45714f9ec7801475c870cb0e2a2de85b76c478
Hiroaki Hosono <m@w125.jp>
2013-07-27 14:05:55 +0900

    change excavator's cost

--- src/main/scala/net/aicomp/terraforming/entity/Installation.scala
+++ src/main/scala/net/aicomp/terraforming/entity/Installation.scala
@@ -4,7 +4,7 @@ object Installation {
 
   val initial = new Installation("initial", 0, 0, 3)
   val robotmaker = new Installation("robotmaker", 50, 4, 3)
-  val excavator = new Installation("excavator", 20, 4, 3)
+  val excavator = new Installation("excavator", 25, 4, 3)
   val tower = new Installation("tower", 25, 5, 3)
   val bridge = new Installation("bridge", 15, 4, 3)
   val house = new Installation("house", 10, 4, 3)
